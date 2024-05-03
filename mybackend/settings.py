b80d6d6d831a325f59c010e4806524d28db1d617
RiturajAgrahari <agr.rituraj@gmail.com>
2024-05-03 14:00:31 +0530

    vercel app is added to cors

--- mybackend/settings.py
+++ mybackend/settings.py
@@ -139,6 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',  # Add the origin of your React app
+    'https://riturajagrahari.vercel.app/',
 ]
 
 # Optional: Allow credentials (cookies, authorization headers, etc.) to be sent
